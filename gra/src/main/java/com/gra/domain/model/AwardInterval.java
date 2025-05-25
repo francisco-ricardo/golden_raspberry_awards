d484f9c33277ecc4b60ce8ed64ea6277f0bbdbbf
Francisco Ricardo <franciscoricardo.dev@gmail.com>
2025-05-25 17:47:13 -0300

    Implement the endpoint /awards
     Changes to be committed:
    	deleted:    gra/src/main/java/com/gra/api/GoldenRaspberryResource.java
    	deleted:    gra/src/main/java/com/gra/api/MyEntity.java
    	modified:   gra/src/main/java/com/gra/domain/model/AwardInterval.java
    	deleted:    gra/src/test/java/com/gra/api/GoldenRaspberryResourceIT.java
    	deleted:    gra/src/test/java/com/gra/api/GoldenRaspberryResourceTest.java

--- gra/src/main/java/com/gra/domain/model/AwardInterval.java
+++ gra/src/main/java/com/gra/domain/model/AwardInterval.java
@@ -1,11 +1,32 @@
 package com.gra.domain.model;
 
-import lombok.Value;
-
-@Value
 public class AwardInterval {
-    String producer;
-    int interval;
-    int previousWin;
-    int followingWin;
+    private String producer;
+    private int interval;
+    private int previousWin;
+    private int followingWin;
+
+    public AwardInterval(String producer, int interval, int previousWin, int followingWin) {
+        this.producer = producer;
+        this.interval = interval;
+        this.previousWin = previousWin;
+        this.followingWin = followingWin;
+    }
+
+    // Getters...
+    public String getProducer() {
+        return this.producer;
+    }
+    
+    public int getInterval() {
+        return this.interval;        
+    }
+
+    public int getPreviousWin() {
+        return this.previousWin;
+    }
+
+    public int getFollowingWin() {
+        return this.followingWin;
+    }
 }
--- gra/src/test/java/com/gra/api/GoldenRaspberryResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.gra.api;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GoldenRaspberryResourceIT extends GoldenRaspberryResourceTest {
-    // Execute the same tests but in packaged mode.
-}
